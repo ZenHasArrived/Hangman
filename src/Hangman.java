e5fc371ffaae2f4d94ffe328b715614f776e62fb
Lucas <lucasp2425@gmail.com>
2025-11-13 13:23:21 -0500

    Hangman class created

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,2 +1,46 @@
+import java.util.Random;
 public class Hangman {
+    String secretWord, userGuess;
+    Random rnd = new Random();
+    int rndNum;
+
+    public Hangman(){
+
+        rndNum = rnd.nextInt(5)+1;
+        if(rndNum==1){
+            secretWord="Gas Station";
+        }
+        else if(rndNum==2){
+            secretWord="Frieren";
+        }
+        else if(rndNum==3){
+            secretWord="Sakuya";
+        }
+        else if(rndNum==4){
+            secretWord="Cybersecurity";
+        }
+        else{
+            secretWord="Space Station";
+        }
+
+        userGuess = secretWord.replaceAll(".","_");
+
+    }
+
+    public String getSecretWord() {
+        return secretWord;
+    }
+
+    public void setSecretWord(String secretWord) {
+        this.secretWord = secretWord;
+    }
+
+    public String getUserGuess() {
+        return userGuess;
+    }
+
+    public void setUserGuess(String userGuess) {
+        this.userGuess = userGuess;
+    }
+
 }
--- src/PlayGame.java
+++ src/PlayGame.java
@@ -1,7 +1,11 @@
 public class PlayGame {
+
+    public static int gamesWon=0, gamesLost=0;
     public static void main(String[] args){
 
-        System.out.print("Hello world placeholder slop");
+        Hangman hng = new Hangman();
+
+        System.out.print(hng.getUserGuess());
 
     }
 
